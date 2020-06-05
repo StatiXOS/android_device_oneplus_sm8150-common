13df8770991e60c8b71a82b0ea3096fc77a620e7
Alexander Koskovich <zvnexus@outlook.com>
2020-06-05 12:36:32 -0400

    sm8150: Use full thermal stack from stock.
     * OEM's generally have a better idea of how to configure
       thermal parameters than most people.
    
    Change-Id: I475e25ee3751928393ccd04a7227bd9695639213

--- common.mk
+++ common.mk
@@ -447,8 +447,7 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.msmnile
+    android.hardware.thermal@1.0-service
 
 # tri-state key
 PRODUCT_PACKAGES += \
