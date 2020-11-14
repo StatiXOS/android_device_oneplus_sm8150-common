3d6405a1408d8f52eb8253c9fe124407342db45f
Filippo <zzkw35@gmail.com>
2020-11-14 13:43:01 +0100

    Revert "sm8150-common: Prebuild librmnetctl"
    This reverts commit 609b0b122280823a57692248d92d8ca9c68eb1fb.
    
    Change-Id: I3dd28a053149a7c110ddcdc2ebf45a9e35448d85

--- common.mk
+++ common.mk
@@ -402,6 +402,7 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
     libprotobuf-cpp-full \
+    librmnetctl \
     libxml2
 
 # Vendor libstdc++
