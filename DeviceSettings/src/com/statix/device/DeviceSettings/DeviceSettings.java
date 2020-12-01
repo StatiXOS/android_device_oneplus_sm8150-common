0e4949a70b6bb4fe275901f8bdc49463314b3774
Ido Ben-Hur <idoybh2@gmail.com>
2020-12-02 01:25:18 +0200

    sm8150-common: DeviceSettings: Improve refresh rate switches
    Have the preference change listeners inside DeviceSettings -
    This allows us to always correctly display forced 90Hz peak rate state.
    What happened before is that you could enable and disable auto refresh rate
    and forced peak would retain its state, even though practically it's disabled now.
    
    Also correctly check if forced peak refresh rate is currently enabled -
    We need to check both min & max refresh rate settings values
    
    Both logic errors together caused a state that forced peak refresh rate always shows enabled,
    even if you fully reopen the app, but truly is disabled and needs re-setting to actually work!
    
    Change-Id: Id87a265755d19f1f9e6318b850f1644a2359d0ea

--- DeviceSettings/src/com/statix/device/DeviceSettings/DeviceSettings.java
+++ DeviceSettings/src/com/statix/device/DeviceSettings/DeviceSettings.java
@@ -47,8 +47,6 @@ import com.android.internal.util.statix.FileUtils;
 
 public class DeviceSettings extends PreferenceFragment {
 
-
-
     private static final String KEY_CATEGORY_REFRESH = "refresh";
     public static final String KEY_REFRESH_RATE = "refresh_rate";
     public static final String KEY_AUTO_REFRESH_RATE = "auto_refresh_rate";
@@ -64,25 +62,39 @@ public class DeviceSettings extends PreferenceFragment {
         addPreferencesFromResource(R.xml.main);
         getActivity().getActionBar().setDisplayHomeAsUpEnabled(true);
 
+        boolean autoRefresh = AutoRefreshRateSwitch.isCurrentlyEnabled(this.getContext());
         mAutoRefreshRate = (SwitchPreference) findPreference(KEY_AUTO_REFRESH_RATE);
-        mAutoRefreshRate.setChecked(AutoRefreshRateSwitch.isCurrentlyEnabled(this.getContext()));
-        mAutoRefreshRate.setOnPreferenceChangeListener(new AutoRefreshRateSwitch(getContext()));
+        mAutoRefreshRate.setChecked(AutoRefreshRateSwitch.isCurrentlyEnabled(autoRefresh);
+        mAutoRefreshRate.setOnPreferenceChangeListener(this);
 
         mRefreshRate = (TwoStatePreference) findPreference(KEY_REFRESH_RATE);
-        mRefreshRate.setEnabled(!AutoRefreshRateSwitch.isCurrentlyEnabled(this.getContext()));
         mRefreshRate.setChecked(RefreshRateSwitch.isCurrentlyEnabled(this.getContext()));
-        mRefreshRate.setOnPreferenceChangeListener(new RefreshRateSwitch(getContext()));
+        mRefreshRate.setOnPreferenceChangeListener(this);
+        updateRefreshRateState(autoRefresh);
 
     }
 
     @Override
-    public boolean onPreferenceTreeClick(Preference preference) {
-        if (preference == mAutoRefreshRate) {
-              mRefreshRate.setEnabled(!AutoRefreshRateSwitch.isCurrentlyEnabled(this.getContext()));
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+         if (preference == mAutoRefreshRate) {
+            Boolean enabled = (Boolean) newValue;
+            Settings.System.putFloat(getContext().getContentResolver(),
+                    Settings.System.PEAK_REFRESH_RATE, 90f);
+            Settings.System.putFloat(getContext().getContentResolver(),
+                    Settings.System.MIN_REFRESH_RATE, 60f);
+            Settings.System.putInt(getContext().getContentResolver(),
+                    AutoRefreshRateSwitch.SETTINGS_KEY, enabled ? 1 : 0);
+            updateRefreshRateState(enabled);
+        } else if (preference == mRefreshRate) {
+            Boolean enabled = (Boolean) newValue;
+            RefreshRateSwitch.setPeakRefresh(getContext(), enabled);
+        } else {
+            Constants.setPreferenceInt(getContext(), preference.getKey(),
+                    Integer.parseInt((String) newValue));
         }
-        return super.onPreferenceTreeClick(preference);
+        return true;
     }
-    
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
